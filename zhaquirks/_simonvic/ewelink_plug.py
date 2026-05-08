f2a38667b5ab0293f70b37ba929896dfb4bb304f
simonvic <simonvic.dev@gmail.com>
2026-05-08 14:08:31 +0200

    refactor(my_quirks): remove variants

--- zhaquirks/_simonvic/ewelink_plug.py
+++ zhaquirks/_simonvic/ewelink_plug.py
@@ -1,97 +1,76 @@
 import zigpy.types as t
-from typing import Final
 
-from zigpy.quirks import CustomCluster
-from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
+from zhaquirks import LocalDataCluster
+
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl import ClusterType
 from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef
-
-from zigpy.quirks.v2 import SensorStateClass, SensorDeviceClass, QuirkBuilder
-from zigpy.quirks.v2.homeassistant import (
-    UnitOfElectricCurrent,
-    UnitOfPower,
-    UnitOfElectricPotential,
-)
+from zigpy.zcl.clusters.homeautomation import ElectricalMeasurement
 
 
-class EWeLinkElectricalMeasurement(CustomCluster, ElectricalMeasurement):
-    """eWeLink electrical measurement"""
-
-    AC_VOLTAGE_MULTIPLIER = 0x0600
-    AC_VOLTAGE_DIVISOR = 0x0601
-    AC_CURRENT_MULTIPLIER = 0x0602
-    AC_CURRENT_DIVISOR = 0x0603
-    AC_POWER_MULTIPLIER = 0x0604
-    AC_POWER_DIVISOR = 0x0605
-
-    _CONSTANT_ATTRIBUTES = {
-        AC_VOLTAGE_MULTIPLIER: 1,
-        AC_VOLTAGE_DIVISOR: 1000,
-        AC_CURRENT_MULTIPLIER: 1,
-        AC_CURRENT_DIVISOR: 1000,
-        AC_POWER_MULTIPLIER: 1,
-        AC_POWER_DIVISOR: 1000,
-    }
+class EWeLinkElectricalMeasurement(LocalDataCluster):
+    """Custom eWeLink plug cluster"""
 
     cluster_id = 0xFC11
 
     class AttributeDefs(BaseAttributeDefs):
-        """Custom eWeLink attributes definitions."""
+        """Custom eWeLink attributes"""
 
-        # rms_current
-        current_reported: Final = ZCLAttributeDef(id=0x7004, type=t.uint16_t)
-        # rms_voltage
-        voltage_reported: Final = ZCLAttributeDef(id=0x7005, type=t.uint16_t)
-        # active_power
-        power_reported: Final = ZCLAttributeDef(id=0x7006, type=t.uint16_t)
+        current_reported = ZCLAttributeDef(
+            id=0x7004,
+            type=t.uint16_t,
+            is_manufacturer_specific=True
+        )
+
+        voltage_reported = ZCLAttributeDef(
+            id=0x7005,
+            type=t.uint32_t,
+            is_manufacturer_specific=True,
+        )
+
+        power_reported = ZCLAttributeDef(
+            id=0x7006,
+            type=t.uint32_t,
+            is_manufacturer_specific=True,
+        )
 
     def _update_attribute(self, attrid, value):
         super()._update_attribute(attrid, value)
-        if attrid == EWeLinkElectricalMeasurement.AttributeDefs.current_reported.id:
-            self.endpoint.electrical_measurement._update_attribute(
-                ElectricalMeasurement.AttributeDefs.rms_current.id, value
-            )
-        elif attrid == EWeLinkElectricalMeasurement.AttributeDefs.voltage_reported.id:
-            self.endpoint.electrical_measurement._update_attribute(
-                ElectricalMeasurement.AttributeDefs.rms_voltage.id, value
-            )
-        elif attrid == EWeLinkElectricalMeasurement.AttributeDefs.power_reported.id:
-            self.endpoint.electrical_measurement._update_attribute(
-                ElectricalMeasurement.AttributeDefs.active_power.id, value
-            )
+        match attrid:
+            case EWeLinkElectricalMeasurement.AttributeDefs.current_reported.id:
+                self.endpoint.electrical_measurement._update_attribute(
+                    ElectricalMeasurement.AttributeDefs.rms_current.id,
+                    value
+                )
+            case EWeLinkElectricalMeasurement.AttributeDefs.voltage_reported.id:
+                self.endpoint.electrical_measurement._update_attribute(
+                    ElectricalMeasurement.AttributeDefs.rms_voltage.id,
+                    t.uint16_t(value / 1000)
+                )
+            case EWeLinkElectricalMeasurement.AttributeDefs.power_reported.id:
+                self.endpoint.electrical_measurement._update_attribute(
+                    ElectricalMeasurement.AttributeDefs.active_power.id,
+                    t.uint16_t(value / 1000)
+                )
+
+
+class ElectricalMeasurementCluster(LocalDataCluster, ElectricalMeasurement):
+
+    _CONSTANT_ATTRIBUTES = {
+        ElectricalMeasurement.AttributeDefs.ac_current_multiplier.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_current_divisor.id: 100,
+        ElectricalMeasurement.AttributeDefs.ac_voltage_multiplier.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_voltage_divisor.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_power_multiplier.id: 1,
+        ElectricalMeasurement.AttributeDefs.ac_power_divisor.id: 1,
+    }
 
 
 (
     QuirkBuilder("eWeLink", "CK-BL702-SWP-01(7020)")
-    .adds(EWeLinkElectricalMeasurement, endpoint_id=1)
-    # .sensor(
-    #     endpoint_id=1,
-    #     cluster_id=EWeLinkElectricalMeasurement.cluster_id,
-    #     attribute_name=EWeLinkElectricalMeasurement.AttributeDefs.current_reported.name,
-    #     state_class=SensorStateClass.MEASUREMENT,
-    #     device_class=SensorDeviceClass.CURRENT,
-    #     unit=UnitOfElectricCurrent.AMPERE,
-    #     divisor=1000,
-    #     fallback_name="Current",
-    # )
-    # .sensor(
-    #     endpoint_id=1,
-    #     cluster_id=EWeLinkElectricalMeasurement.cluster_id,
-    #     attribute_name=EWeLinkElectricalMeasurement.AttributeDefs.voltage_reported.name,
-    #     state_class=SensorStateClass.MEASUREMENT,
-    #     device_class=SensorDeviceClass.VOLTAGE,
-    #     unit=UnitOfElectricPotential.VOLT,
-    #     divisor=1000,
-    #     fallback_name="Voltage",
-    # )
-    # .sensor(
-    #     endpoint_id=1,
-    #     cluster_id=EWeLinkElectricalMeasurement.cluster_id,
-    #     attribute_name=EWeLinkElectricalMeasurement.AttributeDefs.power_reported.name,
-    #     state_class=SensorStateClass.MEASUREMENT,
-    #     device_class=SensorDeviceClass.POWER,
-    #     unit=UnitOfPower.WATT,
-    #     divisor=1000,
-    #     fallback_name="Power",
-    # )
+    # .firmware_version_filter(min_version=0x00001002)
+    .removes(ElectricalMeasurement.cluster_id, cluster_type=ClusterType.Client)
+    .adds(ElectricalMeasurementCluster)
+    .adds(EWeLinkElectricalMeasurement)
     .add_to_registry()
 )
